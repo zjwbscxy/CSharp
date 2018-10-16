237ecdac4035a52f53e8d7bc2f89aeb97b8bc02d
zjwbscxy <4616129975@qq.com>
2018-10-16 20:53:55 +0800

    new file

--- test2/test2/test2/Program.cs
+++ test2/test2/test2/Program.cs
@@ -12,14 +12,14 @@ namespace test2
     class Program
     {
         static void Main(string[] args)
-        {   
-            string fileName = @"E:\test2\科研细则.docx";//将地址存储到字符串fileName
-            using (WordprocessingDocument myDocument = WordprocessingDocument.Open(fileName, true))
+        {
+            string fileName = @"E:\CSharp\CSharp\test2\科研细则.docx";//将地址存储到字符串fileName
+            using (WordprocessingDocument myDocument = WordprocessingDocument.Open(fileName, true))//从WordprocessingDocument类中创建一个新的实例
             {
-                Body word = myDocument.MainDocumentPart.Document.Body;//将科研细则内的正文存储到word
-                foreach (var duan in word.Elements<Paragraph>())
+                Body word = myDocument.MainDocumentPart.Document.Body;//创建一个Body类
+                foreach (var duan in word.Elements<Paragraph>())//遍历Body类Elements的对象
                 {
-                    Console.WriteLine(duan.InnerText);//将word中存储的正文逐行打印
+                    Console.WriteLine(duan.InnerText);//将对象中存储的正文打印
                 }
             }
             Console.WriteLine("All done. Press a key.");//程序已经跑完，按任意按键退出
