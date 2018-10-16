bd0365f683f52eea0c30fa3cfeecdc99187ff234
zjwbscxy <4616129975@qq.com>
2018-10-16 16:28:53 +0800

    new file

---
+++ test2/test2/test2/Program.cs
@@ -0,0 +1,29 @@
+﻿using DocumentFormat.OpenXml.Office2010.Word;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string fileName = @"E:\test2\科研细则.docx";//将地址存储到字符串fileName
+            using (WordprocessingDocument myDocument = WordprocessingDocument.Open(fileName, true))
+            {
+                Body word = myDocument.MainDocumentPart.Document.Body;//将科研细则内的正文存储到word
+                foreach (var duan in word.Elements<Paragraph>())
+                {
+                    Console.WriteLine(duan.InnerText);//将word中存储的正文逐行打印
+                }
+            }
+            Console.WriteLine("All done. Press a key.");//程序已经跑完，按任意按键退出
+            Console.ReadKey();
+        }
+    }
+}
